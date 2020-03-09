99354718b9e02d4a47b32f704f66a4175fbd63d6
lo63tor <fleming.kretschmer@uni-jena.de>
2020-03-09 11:34:38 +0100

    scripts for randomly picking fragments of genomic DNA
    - class for efficient access to many genomes
    - script for picking fragments randomly from a set of genomes with
      regards to phylogenetic/taxonomic balance

---
+++ preprocessing/genome_db.py
@@ -0,0 +1,114 @@
+from os.path import getsize, basename
+from Bio import SeqIO
+from random import choice
+from logging import info
+from time import time
+
+
+class GenomeDB:
+    """has members:
+    seq_dict: SeqIO dictionary accessible by accession
+    spec_dict: dictionary mapping species taxids to list of accessions
+    """
+    def __init__(self, fasta_or_fastas, mapping=None, size_thr=16e9,
+                 name='untitled'):
+        """creates database of sequences accessible by taxid
+
+        :param fasta_or_fastas: either path to a multifasta or paths
+                                to multiple fastas
+        :param str mapping: file containing mapping of IDs to taxids
+                            if None, assumes that IDs are already taxids
+        :param size_thr: cumulative file size below which sequences are
+                         stored in memory
+        """
+        def key_function(id_):
+            # use accession(ref) or whole id
+            if (isinstance(id_, SeqIO.SeqRecord)):
+                id_ = id_.id
+            ids = id_.split('|')
+            if len(ids) < 4 or ids[2] != 'ref':
+                return id_
+            else:
+                return ids[3]
+        self.name = name
+        self.spec_dict = (self.read_mapping(mapping)
+                          if mapping is not None
+                          else None)
+        fastas = ([fasta_or_fastas] if isinstance(fasta_or_fastas, str)
+                  else fasta_or_fastas)
+        info('determining total size of fasta(s)...')
+        total_size = sum([getsize(fasta) for fasta in fastas])
+        if (total_size > size_thr):
+            info(f'size {total_size/1024**3:.2f}GB above threshold '
+                 f'({size_thr/1024**3:.2f}GB), creating index')
+            start = time()
+            self.seq_dict = SeqIO.index_db(basename(fastas[0]) + '.idx',
+                                           fastas,
+                                           format='fasta',
+                                           key_function=key_function)
+            info(f'index created in {time() - start:.2f} seconds')
+        else:
+            info(f'size {total_size/1024**3:.2f}GB below threshold '
+                 f'({size_thr/1024**3:.2f}GB), storing all sequences in '
+                 'memory')
+            start = time()
+            self.seq_dict = dict(item for fasta in fastas
+                                 for item in
+                                 SeqIO.to_dict(
+                                     SeqIO.parse(open(fasta), 'fasta'),
+                                     key_function=key_function).items())
+            info(f'sequence dictionary created in {time() - start:.2f} '
+                 'seconds')
+
+    def read_mapping(self, mapping_file):
+        """reads a mapping from a file where each line has the form:
+        <id> <taxid>
+
+        :param str mapping_file: filepath to read from
+        :return" dictionary mapping IDs to taxids
+        """
+        mapping = {}
+        with open(mapping_file) as f:
+            for line in f:
+                line = line.strip().split()
+                if (len(line) < 2):
+                    continue
+                mapping.setdefault(int(line[1]), []).append(line[0])
+        return mapping
+
+    def all_seqs(self, taxid):
+        """returns all SeqRecords stored for the given species taxid
+
+        :param taxid: species taxid
+        :returns: associated SecRecords
+        """
+        if (self.spec_dict is None):
+            return [self.seq_dict[str(taxid)]]
+        else:
+            return [self.seq_dict[id_] for id_ in self.spec_dict[taxid]]
+
+    def __getitem__(self, idx):
+        """returns a stored sequence for the given taxid
+
+        NOTE: when more than one sequence is stored for a given taxid,
+        one of these is chosen and returned randomly
+
+        :param idx: species taxid
+        :return: SeqRecord associated with taxid
+        """
+        seqids = (self.spec_dict[idx] if self.spec_dict is not None
+                  else [str(idx)])
+        return self.seq_dict[choice(seqids)]
+
+    def __len__(self):
+        """number of species with sequences stored
+        NOTE: there might be more sequences than species!
+
+        :returns: number of species
+        """
+        return len(self.spec_dict if self.spec_dict is not None
+                   else self.seq_dict)
+
+    def __iter__(self):
+        """iterate over species taxids"""
+        return iter(self.spec_dict)
---
+++ preprocessing/genome_mince.py
@@ -0,0 +1,209 @@
+if __name__ == '__main__' and __package__ is None:
+    from os import sys, path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from random import randint, choice, sample
+from preprocessing.process_inputs import seq2kmers
+from preprocessing.genome_db import GenomeDB
+from collections import defaultdict
+from datasketch import MinHash
+import numpy as np
+import json
+import logging
+from tqdm import tqdm
+from pprint import pprint
+import os.path
+from threading import Thread
+import argparse
+
+def kmer_profile(seq, k=7):
+    profile = defaultdict(int)
+    for i in range(len(seq) - k + 1):
+        kmer = seq[i:i+k]
+        profile[kmer] += 1
+    return profile
+
+
+def kmer_dist(kmers1, kmers2):
+    sum_ = 0
+    for kmer in set(kmers1).union(set(kmers2)):
+        sum_ += (kmers1[kmer] - kmers2[kmer])**2
+    return np.sqrt(sum_)
+
+
+def kmer_dist_np(kmers1, kmers2):
+    kmers = list(set(kmers1).union(set(kmers2)))
+    return np.linalg.norm(np.array([kmers1[kmer] for kmer in kmers])
+                          - np.array([kmers2[kmer] for kmer in kmers]))
+
+
+def minhash(seq, k, s):
+    m = MinHash(num_perm=s)
+    for word in seq2kmers(seq, k=k, stride=1, pad=False):
+        m.update(word.encode())
+    return tuple(m.digest())
+
+
+def minhash_exists(minhash, minhashes):
+    return minhash in minhashes
+
+
+def get_fragment(seq, fragment_size=500, k=3):
+    """returns a randomly chosen fragment of sequence with given size
+
+    :param SeqRecord seq: SeqRecord from which to extract fragment
+    :param fragment_size: size in kmers of the fragment
+    :param k: number of nucleotides to group to one kmer
+    :returns: fragment of given size or None if fragment_size is greater
+              than sequence size
+    """
+    if (len(seq) // k < fragment_size):
+        return None
+    start = randint(0, len(seq) - (fragment_size * k))
+    end = start + fragment_size * k
+    return seq[start:end]
+    return seq2kmers(str(seq[start:end].seq))
+
+
+def pick_fragment(taxids, profiles, genome_db, profile_fun, similarity_fun,
+                  max_its=100, **get_kwargs):
+    """returns one randomly chosen fragment from the SeqRecords of the given taxids
+
+    :param taxids: ids from which to choose, must have entries in genome_db
+    :param profiles: set of objects to which the result of profile_fun is
+                     compared
+    :param genome_db: database allowing access to the SeqRecord given a taxid
+    :param profile_fun: function which creates a profile of a given sequence
+    :param similarity_fun: function which compares the profile of the fragment
+                           candidate to all other profiles. if True, fragment
+                           will *not* be picked
+    :param max_its: maximum number of attempts until giving up, can be None
+    :param get_kwargs: kwargs to pass to get_fragment
+    :returns: fragment and its kmer-profile; or None if none could be picked
+    """
+    its = 0
+    fragment = None
+    while fragment is None:
+        its += 1
+        if (max_its is not None and its > max_its):
+            return None
+        taxid = choice(taxids)
+        record = genome_db[taxid]
+        fragment = get_fragment(record, **get_kwargs)
+        if fragment is None:
+            continue
+        profile = profile_fun(str(fragment.seq))
+        if (similarity_fun(profile, profiles)):
+            continue
+    return fragment, profile, taxid
+
+
+def get_sk_fragments(nr_fragments, orders_dict, genome_db,
+                     profile_fun, similarity_fun,
+                     max_its=None, order_max_its=None,
+                     thread_nr=None,
+                     **get_fragment_kwargs):
+    """obtains specified number of fragments based on the given order taxids
+
+    :param nr_fragments: number of fragments to obtain
+    :param orders_dict: dictionary, mapping order taxids to species taxids
+    :param genome_db: genomeDB containing genomes of the species taxids
+    :param profile_fun: function which creates a profile of a given sequence
+    :param similarity_fun: function which compares the profile of the fragment
+                           candidate to all other profiles. if True, fragment
+                           will *not* be picked
+    :param max_its: maximum number of iterations until giving up
+    :param order_max_its: maximum number of iterations per order
+    :returns: fragments and kmer-profiles
+    """
+    def finished():
+        if (max_its is not None and iterations >= max_its):
+            logging.warning("maximum iterations reached!")
+            return True
+        return (len(fragments) >= nr_fragments)
+    profiles = []
+    fragments = []
+    speciess = []
+    iterations = 0
+    pbar = tqdm(total=nr_fragments, position=thread_nr,
+                desc=genome_db.name)
+    while not finished():
+        for order in sample(list(orders_dict), len(orders_dict)):
+            if (finished()):
+                break
+            iterations += 1
+            avail_taxids = [taxid for taxid in orders_dict[order]
+                            if taxid in genome_db]
+            if (len(avail_taxids)) == 0:
+                continue
+            picked = pick_fragment(avail_taxids, profiles, genome_db,
+                                   profile_fun, similarity_fun,
+                                   order_max_its, **get_fragment_kwargs)
+            if (picked is None):
+                continue
+            fragment, profile, species = picked
+            fragments.append(fragment)
+            profiles.append(profile)
+            speciess.append(species)
+            pbar.update()
+    pbar.close()
+    print(f'{iterations} iterations on superkingdom level')
+    return fragments, profiles, speciess
+
+
+def load_genomes(genome_dir, sk, thr=16e9):
+    def from_fastadir(sk_dir):
+        fastas = [os.path.join(sk_dir, _.strip())
+                  for _ in open(os.path.join(sk_dir, 'files.txt')).readlines()]
+        return GenomeDB(fastas, os.path.join(sk_dir, 'mapping.tsv'), name=sk, size_thr=thr)
+
+    def from_fasta(fasta_file, mapping):
+        return GenomeDB(fasta_file, mapping, name=sk, size_thr=thr)
+
+    logging.info(f'loading {sk} genomes')
+    if (sk == 'Archaea'):
+        return from_fastadir(
+            os.path.join(genome_dir, 'Archaea'))
+    elif (sk == 'Bacteria'):
+        return from_fasta(
+            os.path.join(genome_dir, 'Bacteria/full_genome_bacteria.fna'),
+            os.path.join(genome_dir, 'mapping_Bacteria.tsv'))
+    elif (sk == 'Eukaryota'):
+        return from_fastadir(
+            os.path.join(genome_dir, 'Eukaryota'))
+    elif (sk == 'Viruses'):
+        return from_fasta(
+            os.path.join(genome_dir, 'Viruses/all_viruses_db.fa'),
+            os.path.join(genome_dir, 'mapping_Viruses.tsv'))
+    else:
+        raise Exception(f'genomes of {sk} not available')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sk')
+    parser.add_argument('nr_fragments', type=int)
+    parser.add_argument('--outdir', '-o', default='.')
+    parser.add_argument('--thr', '-t', type=float, default=16e9)
+    logging.getLogger().setLevel(logging.INFO)
+    args = parser.parse_args()
+    nr_seqs = args.nr_fragments
+    sk = args.sk
+    sk_order_dict = json.load(
+        open('/home/lo63tor/Downloads/new_taxdump/sk_order_dict.json'))
+
+    def minhash_defined(seq):
+        return minhash(seq, 6, 6)
+
+    def sk_fragments_plus_stats(sk, outdir='.', thread_nr=None):
+        genome_db = load_genomes(
+            '/home/lo63tor/master/sequences/dna_sequences/genomes', sk, thr=args.thr)
+        fragments, profiles, speciess = get_sk_fragments(
+            nr_seqs, sk_order_dict[sk], genome_db, minhash_defined,
+            minhash_exists, 10_000_000, None, thread_nr)
+        json.dump([str(seq.seq) for seq in fragments],
+                  open(os.path.join(outdir, f'{sk}_fragments.json'), 'w'))
+        with open(os.path.join(outdir, f'{sk}_species_picked.txt'), 'w') as f:
+            f.writelines(str(sp) + '\n' for sp in speciess)
+    logging.info(f'generating fragments for {sk}')
+    sk_fragments_plus_stats(sk, args.outdir)
+    logging.info(f'[{sk}] done')
