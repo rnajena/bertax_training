b75b7f1335c7f46da71a158e0a0842f46274e8b4
lo63tor <fleming.kretschmer@uni-jena.de>
2020-04-05 18:04:37 +0200

    fixed nc.py, added nonalph-character threshold for genome_mince.py
    - also fixed (potential) bug in genome_mince, accepting fragments
      even when the profile is similar to a stored one

--- models/nc.py
+++ models/nc.py
@@ -4,28 +4,32 @@ if __name__ == '__main__' and __package__ is None:
 import numpy as np
 import json
 import os.path
-from random import shuffle
+from random import shuffle, sample
 from sklearn.model_selection import train_test_split
 from preprocessing.process_inputs import encode_sequence, ALPHABET, get_class_vectors, words2index
-import keras
 from models.model import PARAMS, DCModel
 from argparse import ArgumentParser
 from itertools import product
 from keras.callbacks import ModelCheckpoint
-from keras.utils import Sequence
+from tensorflow.python.keras.utils.data_utils import Sequence
 from logging import warning
 
 classes = PARAMS['data']['classes'][1]
 
 
-def load_fragments(fragments_dir, shuffle_=True):
+def load_fragments(fragments_dir, shuffle_=True, nr_seqs=None):
     x = []
     y = []
     for class_ in classes:
-        for fragment in json.load(open(os.path.join(
-            fragments_dir, f'{class_}_fragments.json'))):
-         x.append(fragment)
-         y.append(class_)
+        fragments = json.load(open(os.path.join(
+            fragments_dir, f'{class_}_fragments.json')))
+        if (nr_seqs is not None):
+            if (not shuffle_):
+                warning('fragments *will* be shuffled because nr_seqs is specified')
+            fragments = sample(fragments, min(nr_seqs, len(fragments)))
+        for fragment in fragments:
+            x.append(fragment)
+            y.append(class_)
     if (shuffle_):
         to_shuffle = list(zip(x, y))
         shuffle(to_shuffle)
@@ -51,12 +55,12 @@ class FragmentGenerator(Sequence):
         batch_fragments = self.x[idx * batch_size:
                                  (idx+1) * batch_size]
         batch_classes = self.y[idx * batch_size:
-                              (idx+1) * batch_size]
+                               (idx+1) * batch_size]
         batch_y = np.array([self.class_vectors[c] for c in batch_classes])
-        batch_x = [encode_sequence(seq, 'window', words2index, k=3, stride=3,
-                                      max_seq_len=self.seq_len)
-                      for seq in batch_fragments]
-        return batch_x, batch_y
+        batch_x = np.array([np.array(encode_sequence(seq, 'window', words2index, k=3, stride=3,
+                                                max_seq_len=self.seq_len, handle_nonalph='special'))
+                            for seq in batch_fragments])
+        return (batch_x, batch_y)
 
 if __name__ == '__main__':
     parser = ArgumentParser(
@@ -67,18 +71,18 @@ if __name__ == '__main__':
     parser.add_argument('--epochs', type=int, default=50)
     args = parser.parse_args()
     # args = parser.parse_args(['/home/lo63tor/master/dna_class/output/genomic_fragments/',
-    #                           '--seq_len', '502', '--batch_size', '70', '--head_num', '2',
-    #                           '--transformer_num', '2', '--embed_dim', '10', '--epochs', '3'])
+    #                           '--epochs', '3'])
     batch_size = args.batch_size
     # building model
     model = DCModel(classes, args.seq_len, 65, summary=True)
-    model.generate_cnndeep_predef_model(dropout_rate=0)
+    model.generate_tcn_model(1, dilations=[2**i for i in range(8)])
     # loading training data
-    x, y = load_fragments(args.fragments_dir)
+    x, y = load_fragments(args.fragments_dir, nr_seqs=10_000)
     f_train_x, f_test_x, f_train_y, f_test_y = train_test_split(
         x, y, test_size=0.2)
     f_train_x, f_val_x, f_train_y, f_val_y = train_test_split(
         f_train_x, f_train_y, test_size=0.05)
+    # f = FragmentGenerator(f_val_x, f_val_y, args.seq_len)
     model.train(FragmentGenerator(f_train_x, f_train_y, args.seq_len),
                 FragmentGenerator(f_val_x, f_val_y, args.seq_len),
                 batch_size=batch_size, epochs=args.epochs)
